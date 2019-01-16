6bce492be0e9e8e5f8884ff6dfa7652e260fa6ec
deltamaze <wcpooley@gmail.com>
2019-01-15 23:15:43 -0600

    deployed to linux server, test passed

--- HelloWorldTestDeploy/HelloWorldTestDeploy/Program.cs
+++ HelloWorldTestDeploy/HelloWorldTestDeploy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NLog;
 
 namespace HelloWorldTestDeploy
 {
@@ -6,7 +7,19 @@ namespace HelloWorldTestDeploy
     {
         static void Main(string[] args)
         {
+            //purpose push to terminal ubuntu server, test input, output, log writing
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter Key value to test read key.");
+            string input = Console.ReadKey().KeyChar.ToString();
+            Console.WriteLine(String.Format("\ninput value: {0}",input));
+
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Trace("TraceTest");
+            logger.Info("InfoTest");
+            logger.Error("ErrorTest");
+
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
